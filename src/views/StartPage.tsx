1343a623c908dd3adbc9cd555f791c1f4ceba103
aliyu <1324149139@qq.com>
2022-11-01 15:12:25 +0800

    制作center组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,7 +1,10 @@
-import { defineComponent } from 'vue';
-import { Button } from '../shared/Button';
-import { FloatButton } from '../shared/FloatButton';
+import {defineComponent} from 'vue';
+import {Button} from '../shared/Button';
+import {FloatButton} from '../shared/FloatButton';
 import s from './StartPage.module.scss';
+import {Center} from "../shared/Center";
+import {Icon} from "../shared/Icon";
+
 export const StartPage = defineComponent({
   setup: (props, context) => {
     const onClick = () => {
@@ -9,6 +12,10 @@ export const StartPage = defineComponent({
     }
     return () => (
       <div>
+        <nav>menu</nav>
+        <Center class={s.pig_wrapper}>
+          <Icon name='pig' class={s.pig}/>
+        </Center>
         <div class={s.button_wrapper}>
           <Button class={s.button} onClick={onClick}>测试</Button>
         </div>
