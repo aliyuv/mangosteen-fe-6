3936dcee573a1c0cdc7ff80bed197c9154e29a01
aliyu <1324149139@qq.com>
2022-11-01 15:47:33 +0800

    添加 Navbar 组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -4,20 +4,25 @@ import {FloatButton} from '../shared/FloatButton';
 import s from './StartPage.module.scss';
 import {Center} from "../shared/Center";
 import {Icon} from "../shared/Icon";
+import {Navbar} from "../shared/Navbar";
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
-    const onClick = () => {
-      console.log('hi')
-    }
     return () => (
       <div>
-        <nav>menu</nav>
+        <Navbar>
+          {
+            {
+              default: () => '山竹记账',
+              icon: () => <Icon name='menu' class={s.navIcon}/>
+            }
+          }
+        </Navbar>
         <Center class={s.pig_wrapper}>
           <Icon name='pig' class={s.pig}/>
         </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button} onClick={onClick}>测试</Button>
+          <Button class={s.button}>开始记账</Button>
         </div>
         <FloatButton iconName='add'/>
       </div>
