ea549b2ad1937c1d79d827230be562c12d1d5384
aliyu <1324149139@qq.com>
2022-11-03 00:57:22 +0800

    完成数字输入功能 验证规则

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -17,8 +17,34 @@ export const InputPad = defineComponent({
     const showDatePicker = () => refDatePickerVisible.value = true
     const hideDatePicker = () => refDatePickerVisible.value = false
     const setDate = (date: Date) => {refDate.value = date; hideDatePicker()}
-    const refAmount = ref('')
-    const appendText = (n: number | string) => refAmount.value += n.toString()
+    const refAmount = ref('0')
+    const appendText = (n: number | string) => {
+      const nString = n.toString()
+      const doIndex = refAmount.value.indexOf('.')
+      if(refAmount.value.length >= 13){
+        return
+      }
+      if(doIndex >= 0 && refAmount.value.length - doIndex > 2){
+        return
+      }
+      if(nString === '.'){
+        if(doIndex >= 0){ //已经有小数点了
+          return
+        }
+      }else if(nString === '0'){
+        if(doIndex === -1){ //没有小数点
+          if (refAmount.value === '0'){ //没有小数点，且第一位是0
+            return
+          }
+        }
+      }else {
+        if (refAmount.value === '0'){ //第一位是0
+          refAmount.value = ''
+        }
+      }
+
+      refAmount.value += n.toString()
+    }
     const buttons = [
       {text: '1', onClick: () => {appendText(1)}},
       {text: '2', onClick: () => {appendText(2)}},
@@ -31,7 +57,7 @@ export const InputPad = defineComponent({
       {text: '9', onClick: () => {appendText(9)}},
       {text: '0', onClick: () => {appendText(0)}},
       {text: '.', onClick: () => {appendText('.')}},
-      {text: '清空', onClick: () => {refAmount.value = ''}},
+      {text: '清空', onClick: () => {refAmount.value = '0'}},
       {text: '提交', onClick: () => {}},
     ]
     return () => <>
