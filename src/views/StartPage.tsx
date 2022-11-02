8db2a6e8b8aebfc3847ce5441e55ba0a69f994c4
aliyu <1324149139@qq.com>
2022-11-02 13:51:43 +0800

    完成 MainLayout 组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -4,9 +4,9 @@ import {FloatButton} from '../shared/FloatButton';
 import s from './StartPage.module.scss';
 import {Center} from "../shared/Center";
 import {Icon} from "../shared/Icon";
-import {Navbar} from "../shared/Navbar";
 import {Overlay} from "../shared/Overlay";
 import {RouterLink} from "vue-router";
+import {MainLayout} from "../layouts/MainLayout";
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
@@ -15,31 +15,29 @@ export const StartPage = defineComponent({
       refOverlayVisible.value = !refOverlayVisible.value
     }
     return () => (
-      <div>
-        <Navbar>
-          {
-            {
-              default: () => '山竹记账',
-              icon: () => <Icon name='menu' class={s.navIcon} onClick={onClickMenu}/>
-            }
-          }
-        </Navbar>
-        <Center class={s.pig_wrapper}>
-          <Icon name='pig' class={s.pig}/>
-        </Center>
-        <div class={s.button_wrapper}>
-          <RouterLink to='/items/create'>
-            <Button class={s.button}>开始记账</Button>
-          </RouterLink>
-        </div>
-        <RouterLink to='/items/create'>
-          <FloatButton iconName='add'/>
-        </RouterLink>
+      <MainLayout>{
         {
-          refOverlayVisible.value &&
-          <Overlay onClose={() => refOverlayVisible.value = false}/>
+          title: () => '山竹记账',
+          icon: () => <Icon name='menu' class={s.navIcon} onClick={onClickMenu}/>,
+          default: () => <>
+            <Center class={s.pig_wrapper}>
+              <Icon name='pig' class={s.pig}/>
+            </Center>
+            <div class={s.button_wrapper}>
+              <RouterLink to='/items/create'>
+                <Button class={s.button}>开始记账</Button>
+              </RouterLink>
+            </div>
+            <RouterLink to='/items/create'>
+              <FloatButton iconName='add'/>
+            </RouterLink>
+            {
+              refOverlayVisible.value &&
+              <Overlay onClose={() => refOverlayVisible.value = false}/>
+            }
+          </>
         }
-      </div>
+      }</MainLayout>
     )
   }
 })
