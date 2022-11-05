b75e00df8bba9a0e66788ac3a22acc3a00bcb10a
aliyu <1324149139@qq.com>
2022-11-05 17:19:54 +0800

    wip: 创建 TagEdit

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,29 +1,28 @@
-import {defineComponent, PropType, reactive, toRaw} from 'vue';
+import {defineComponent, reactive} from 'vue';
 import {MainLayout} from '../../layouts/MainLayout';
 import {Button} from '../../shared/Button';
 import {EmojiSelect} from '../../shared/EmojiSelect';
 import {Icon} from '../../shared/Icon';
-import s from './TagCreate.module.scss';
-import {Rules, validate} from "../../shared/validate";
+import {Rules, validate} from '../../shared/validate';
+import s from './Tag.module.scss';
 
 export const TagCreate = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
   setup: (props, context) => {
     const formData = reactive({
       name: '',
       sign: '',
     })
     const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({})
-    const onsubmit = (e: Event) => {
+    const onSubmit = (e: Event) => {
       const rules: Rules<typeof formData> = [
         {key: 'name', type: 'required', message: '必填'},
         {key: 'name', type: 'pattern', regex: /^.{1,4}$/, message: '只能填 1 到 4 个字符'},
         {key: 'sign', type: 'required', message: '必填'},
       ]
+      Object.assign(errors, {
+        name: undefined,
+        sign: undefined
+      })
       Object.assign(errors, validate(formData, rules))
       e.preventDefault()
     }
@@ -33,7 +32,7 @@ export const TagCreate = defineComponent({
         icon: () => <Icon name="left" onClick={() => {
         }}/>,
         default: () => (
-          <form class={s.form} onSubmit={onsubmit}>
+          <form class={s.form} onSubmit={onSubmit}>
             <div class={s.formRow}>
               <label class={s.formLabel}>
                 <span class={s.formItem_name}>标签名</span>
@@ -41,7 +40,7 @@ export const TagCreate = defineComponent({
                   <input v-model={formData.name} class={[s.formItem, s.input, s.error]}></input>
                 </div>
                 <div class={s.formItem_errorHint}>
-                  <span>{errors['name'] ? errors['name'][0] : ' '}</span>
+                  <span>{errors['name'] ? errors['name'][0] : '　'}</span>
                 </div>
               </label>
             </div>
@@ -52,7 +51,7 @@ export const TagCreate = defineComponent({
                   <EmojiSelect v-model={formData.sign} class={[s.formItem, s.emojiList, s.error]}/>
                 </div>
                 <div class={s.formItem_errorHint}>
-                  <span>{errors['sign'] ? errors['sign'][0] : ' '}</span>
+                  <span>{errors['sign'] ? errors['sign'][0] : '　'}</span>
                 </div>
               </label>
             </div>
--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -41,7 +41,7 @@ export const routes: RouteRecordRaw[] = [
     path: '/tags', component: TagPag,
     children: [
       {path: 'create', component: TagCreate},
-      {path: ':id', component: TagEdit},
+      {path: ':id/edit', component: TagEdit},
     ]
   }
 ]
