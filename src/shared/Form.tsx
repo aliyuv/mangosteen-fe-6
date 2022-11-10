f4a53ef63d748dcf0813b67ec5623f2c0648fbd7
aliyu <1324149139@qq.com>
2022-11-10 14:08:48 +0800

    封装 Form组件

---
+++ src/shared/Form.tsx
@@ -0,0 +1,68 @@
+import {computed, defineComponent, PropType} from "vue";
+import s from './Form.module.scss'
+import {EmojiSelect} from "./EmojiSelect";
+
+export const Form = defineComponent({
+  props: {
+    onSubmit: {
+      type: Function as PropType<(e: Event) => void>,
+    }
+  },
+  setup: (props, context) => {
+    return () => (
+      <form class={s.form} onSubmit={props.onSubmit}>
+        {context.slots.default?.()}
+      </form>
+    )
+  }
+})
+
+export const FormItem = defineComponent({
+  props: {
+    label: {
+      type: String
+    },
+    modelValue: {
+      type: [String, Number]
+    },
+    type: {
+      type: String as PropType<'text' | 'emojiSelect' | 'date'>,
+    },
+    error: {
+      type: String
+    }
+  },
+  setup: (props, context) => {
+    const content = computed(() => {
+      switch (props.type) {
+        case 'text':
+          return <input
+            value={props.modelValue}
+            onInput={(e: any) => context.emit('update:modelValue', e.target.value)}
+            class={[s.formItem, s.input, s.error]}
+          />
+        case 'emojiSelect':
+          return <EmojiSelect
+            modelValue={props.modelValue?.toString()}
+            onUpdateModelValue={value => context.emit('update:modelValue', value)}
+            class={[s.formItem, s.emojiList, s.error]}
+          />
+        case 'date':
+          return <input/>
+        case undefined:
+          return content.slots.default?.()
+      }
+    })
+    return () => {
+      return <div class={s.formRow}>
+        <label class={s.formLabel}>
+          {props.label && <span class={s.formItem_name}>{props.label}</span>}
+          <div class={s.formItem_value}>
+            {content.value}
+          </div>
+          {props.error && <div class={s.formItem_errorHint}>{props.error}</div>}
+        </label>
+      </div>
+    }
+  }
+})
