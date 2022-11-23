8402b6e5f9118196701090897a89347a2a8f7011
aliyu <1324149139@qq.com>
2022-11-23 16:13:20 +0800

    如果看过广告，就不用看了

---
+++ src/shared/SkipFeatures.tsx
@@ -0,0 +1,16 @@
+import {defineComponent} from "vue";
+import s from './SkipFeatures.module.scss'
+import {RouterLink} from "vue-router";
+
+export const SkipFeatures = defineComponent({
+  setup: (props, context) => {
+    const onClick = () => {
+      localStorage.setItem('skipFeatures', 'yes')
+    }
+    return () => (
+      <span onClick={onClick}>
+        <RouterLink to='/start'>跳过</RouterLink>
+      </span>
+    )
+  }
+})
