7d7d3f45d4847ff00d45c478c15e3bdc00bfae03
aliyu <1324149139@qq.com>
2022-11-28 13:34:44 +0800

    注释

--- src/shared/useTags.tsx
+++ src/shared/useTags.tsx
@@ -10,7 +10,8 @@ export const useTags = (fetcher: Fetcher) => {
     const response = await fetcher(page.value)
     const {resources, pager} = response.data
     tags.value.push(...resources)
-    hasMore.value = (pager.page - 1) * pager.per_page + resources.length < pager.count
+    //那之前页的数量不就是 (page-1)*per_page 了吗？ 为什么要用总数减去之前页的数量？ 为什么不直接用 pager.total_count ？ 因为 pager.total_count 是总数，不是总页数 。
+    hasMore.value = (pager.page - 1) * pager.per_page + resources.length < pager.count // 之前页的数量 = 上一页的页数 * 每页的数量 + 当前页的数量 < 总数
     page.value += 1
   }
   onMounted(fetchTags)
