daf0d6633c1120ca6b358e72a82eee128256e93b
aliyu <1324149139@qq.com>
2022-11-30 14:12:40 +0800

    封装Money

---
+++ src/shared/Money.tsx
@@ -0,0 +1,26 @@
+import {defineComponent, PropType} from "vue";
+
+export const Money = defineComponent({
+  props: {
+    value: {
+      type: Number as PropType<number>,
+      required: true
+    }
+  },
+  setup: (props, context) => {
+    const addZero = (n: number) => {
+      const nString = n.toString() // 1 => '1'
+      //找出字符串中的小数点 1.1 => 1 1 => -1
+      const dotIndex = nString.indexOf('.')
+      if (dotIndex < 0) {
+        return nString + '.00'
+        //如果小数点后面的数字是一位的话，就在后面加一个0
+      } else if (nString.substring(dotIndex).length === 2) {
+        return nString + '0'
+      } else {
+        return nString
+      }
+    }
+    return () => (<span>{addZero(props.value / 100)}</span>)
+  }
+})
